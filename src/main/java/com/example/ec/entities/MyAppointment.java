f7176b34072d504cd48be6c4572879a5b086496b
yaselhouari <yas.elhouari@gmail.com>
2020-11-28 22:45:43 +0100

    create the appointment relational table

--- src/main/java/com/example/ec/entities/MyAppointment.java
+++ src/main/java/com/example/ec/entities/MyAppointment.java
@@ -2,6 +2,7 @@ package com.example.ec.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.sql.Time;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +17,33 @@ public class MyAppointment implements Serializable {
 
     @Column
     String time;
+
+    @Column
+    String dateCreated;
+
+    @Column
+    String clientName;
+
+    @Column
+    String clientPhone;
+
+    @Column
+    String startTimeExpected;
+
+    @Column
+    String startTime;
+
+    @Column
+    String endTimeExpected;
+
+    @Column
+    String endTime;
+
+    @Column
+    String price;
+
+    @Column
+    String status;
 
     public MyAppointment() {
     }
@@ -44,12 +72,84 @@ public class MyAppointment implements Serializable {
         this.time = time;
     }
 
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientPhone() {
+        return clientPhone;
+    }
+
+    public void setClientPhone(String clientPhone) {
+        this.clientPhone = clientPhone;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTimeExpected() {
+        return endTimeExpected;
+    }
+
+    public void setEndTimeExpected(String endTimeExpected) {
+        this.endTimeExpected = endTimeExpected;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     @Override
     public String toString() {
         return "MyAppointment{" +
                 "my_client_id=" + my_client_id +
                 ", my_service_id=" + my_service_id +
                 ", time='" + time + '\'' +
+                ", dateCreated='" + dateCreated + '\'' +
+                ", clientName='" + clientName + '\'' +
+                ", clientPhone='" + clientPhone + '\'' +
+                ", startTime='" + startTime + '\'' +
+                ", endTimeExpected='" + endTimeExpected + '\'' +
+                ", endTime='" + endTime + '\'' +
+                ", price='" + price + '\'' +
+                ", status='" + status + '\'' +
                 '}';
     }
 
@@ -60,11 +160,19 @@ public class MyAppointment implements Serializable {
         MyAppointment that = (MyAppointment) o;
         return Objects.equals(my_client_id, that.my_client_id) &&
                 Objects.equals(my_service_id, that.my_service_id) &&
-                Objects.equals(time, that.time);
+                Objects.equals(time, that.time) &&
+                Objects.equals(dateCreated, that.dateCreated) &&
+                Objects.equals(clientName, that.clientName) &&
+                Objects.equals(clientPhone, that.clientPhone) &&
+                Objects.equals(startTime, that.startTime) &&
+                Objects.equals(endTimeExpected, that.endTimeExpected) &&
+                Objects.equals(endTime, that.endTime) &&
+                Objects.equals(price, that.price) &&
+                Objects.equals(status, that.status);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(my_client_id, my_service_id, time);
+        return Objects.hash(my_client_id, my_service_id, time, dateCreated, clientName, clientPhone, startTime, endTimeExpected, endTime, price, status);
     }
 }
